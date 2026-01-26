920117b11f8fcb31687e402dbbac43d70e15905e
jazzkid <jazzkid@jazzkid.xyz>
2026-01-26 17:08:18 +0000

    nixarr input

--- flake.nix
+++ flake.nix
@@ -16,6 +16,10 @@
             url = "github:ryantm/agenix";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+        nixarr = {
+            url = "github:rasmus-kirk/nixarr";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
         nvim-config = {
             url = "github:Jazzkid0/nvim";
             flake = false;
@@ -30,10 +34,10 @@
         };
     };
 
-    outputs = { self, nixpkgs, deploy-rs, home-manager, agenix, ... }@inputs:
+    outputs = { self, nixpkgs, deploy-rs, home-manager, agenix, nixarr, ... }@inputs:
         let
             mkSystem = { hostname, user, modules }: nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs self; };
+                specialArgs = { inherit inputs self agenix nixarr; };
                 modules = [
                     ./hosts/${hostname}/default.nix
                     ./modules/common/common.nix
@@ -57,11 +61,13 @@
                 #     ];
                 # };
 
-                # jazznas = mkSystem {
-                #     hostname = "jazzpc;
-                #     user = "jazzkid";
-                #     modules = [ networking?]
-                # };
+                jazznas = mkSystem {
+                    hostname = "jazznas";
+                    user = "jazzkid";
+                    modules = [
+                        # NAS-specific modules will be added in Phase 5
+                    ];
+                };
 
                 jazzserver = mkSystem {
                     hostname = "jazzserver";
@@ -82,12 +88,12 @@
                         };
                     };
 
-                    # jazznas = {
-                    #     hostname = "nas.jazzkid.xyz";
-                    #     profiles.system = {
-                    #         path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.jazznas;
-                    #     };
-                    # };
+                    jazznas = {
+                        hostname = "nas.jazzkid.xyz";
+                        profiles.system = {
+                            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.jazznas;
+                        };
+                    };
 
                 };
             };
