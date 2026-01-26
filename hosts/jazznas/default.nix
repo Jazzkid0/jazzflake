634550e9853f0fb4a702a24590ffe2f51273852a
jazzkid <jazzkid@jazzkid.xyz>
2026-01-26 17:23:47 +0000

    hosts/jazznas/default.nix

--- hosts/jazznas/default.nix
+++ hosts/jazznas/default.nix
@@ -1,3 +1,27 @@
-{ ... }:
+{ config, lib, pkgs, agenix, nixarr, ... }:
 {
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./hardware/mounts.nix
+      ./hardware/disks.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernel.sysctl."net.ipv4.ip_forward" = true;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = true;
+
+  networking.hostName = "jazznas";
+  networking.networkmanager.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = ["jazzkid"];
+
+  time.timeZone = "Europe/London";
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "24.11"; # DO NOT EDIT
 }
