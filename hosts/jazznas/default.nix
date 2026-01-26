4a93d395e5ebd9803c508e3ea9de0e3c09bb26cf
jazzkid <jazzkid@jazzkid.xyz>
2026-01-26 19:21:16 +0000

    jazznas default.nix users, keys, agenix

--- hosts/jazznas/default.nix
+++ hosts/jazznas/default.nix
@@ -16,12 +16,38 @@
   networking.hostName = "jazznas";
   networking.networkmanager.enable = true;
 
+  services.tailscale.useRoutingFeatures = "both";
+
+  users.users.jazzkid = {
+    isNormalUser = true;
+    description = "jazzkid";
+    extraGroups = [ "networkmanager" "wheel" "media" ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHJpS4qhtDcfcjD0cD0PmnWxay53Y5Xlf0mPOcSdtkL jazzkid@jazzserver"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvWLv+pEt4tnil5IsMrh/BVqRZLbsuOZZ9MycuH8K6n jazzpc"
+    ];
+  };
+
+  users.groups.media = {};
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHJpS4qhtDcfcjD0cD0PmnWxay53Y5Xlf0mPOcSdtkL jazzkid@jazzserver"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvWLv+pEt4tnil5IsMrh/BVqRZLbsuOZZ9MycuH8K6n jazzpc"
+  ];
+
+  programs.mosh.enable = true;
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.trusted-users = ["jazzkid"];
 
   time.timeZone = "Europe/London";
 
   nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = [
+    agenix.packages.${pkgs.system}.default
+  ];
 
   system.stateVersion = "24.11"; # DO NOT EDIT
 }
