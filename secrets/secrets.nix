3c56595c0e64faf150379f52bb430be5686bed04
jazzkid <jazzkid@jazzkid.xyz>
2026-02-08 09:18:32 +0000

    p7

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,3 +1,10 @@
-{ ... }:
+let
+  jazzkid_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIG5nQmbAPLBByAO4JzuhIEKLv5Bwnbz+6MLuiUrv23G jazzkid@jazznas";
+  jazznas_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlJScwuL1jSVfLb6ke0n4mcrG1htSwtziTgBf9CAOVi";
+  jazzserver_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtHbdmyU16pT3LX0/YJjdRVAPLvDNyKeyPm8z1KNSC5";
+  keys = [ jazzkid_key jazznas_key jazzserver_key ];
+in
 {
+  "cloudflare_apiTokenFile.age".publicKeys = keys;
+  "transmission_credentialsFile.age".publicKeys = keys;
 }
