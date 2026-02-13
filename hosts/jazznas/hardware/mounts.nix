237be76a886d2227c6f28666892c1865b020550b
jazzkid <jazzkid@jazzkid.xyz>
2026-02-13 22:51:23 +0000

    fix nas mounts

--- hosts/jazznas/hardware/mounts.nix
+++ hosts/jazznas/hardware/mounts.nix
@@ -1,7 +1,15 @@
 {
+  # fileSystems."/srv/media" = {
+  #   device = "/dev/disk/by-uuid/1EA635EBA635C455";
+  #   fsType = "ntfs-3g"; # TODO: reformat to btrfs when next disk is installed
+  #   options = [ "uid=1001" "gid=992" "umask=0002" ];
+  # };
   fileSystems."/srv/media" = {
-    device = "/dev/disk/by-uuid/1EA635EBA635C455";
-    fsType = "ntfs-3g"; # TODO: reformat to btrfs when next disk is installed
-    options = [ "uid=1001" "gid=992" "umask=0002" ];
+    device = "/dev/disk/by-uuid/1d3b0924-b12b-4598-83f8-1c093d8b779e";
+    fsType = "btrfs";
+    options = [
+      "compress=zstd"
+      "autodefrag"
+    ];
   };
 }
