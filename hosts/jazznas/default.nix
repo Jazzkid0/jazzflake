50da325ad1ad2888db7c448ce7e433978d2c9943
jazzkid <jazzkid@jazzkid.xyz>
2026-02-15 06:20:51 +0000

    jazztab key

--- hosts/jazznas/default.nix
+++ hosts/jazznas/default.nix
@@ -46,6 +46,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQVZnbcapMI2cVH7/t7WYCcxDbVyQQYIWW4Q51K5CIO jazzkid@jazzserver"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbpMhMLZqxJXZxKF8PsNtjV69h2HbRwA4HFRAURqjB4 jazzkid@jazznas"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7Ri38NEb1cN4BA6xQzsqpGrG1VaiRjZxS+D21UV1RA jazzkid@jazzphone"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCRD3bZT/rqSSmteMz58X1di54tbHiOQTx3G8wXbezQ jazzkid@jazztab"
     ];
   };
 
