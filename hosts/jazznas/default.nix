9982ae02f5e35163b09d1d31ef2d3dfda7bedc7b
jazzkid <jazzkid@jazzkid.xyz>
2026-03-12 02:32:39 +0000

    jazzpc root pubkey

--- hosts/jazznas/default.nix
+++ hosts/jazznas/default.nix
@@ -57,6 +57,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXr41H8R6YjEFGilGFw3k+KmuPyDaOofxctpQMmY18f jazzkid@jazzsl"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQVZnbcapMI2cVH7/t7WYCcxDbVyQQYIWW4Q51K5CIO jazzkid@jazzserver"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbpMhMLZqxJXZxKF8PsNtjV69h2HbRwA4HFRAURqjB4 jazzkid@jazznas"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIN3rt+uOSH4HrmG/LiNb3r7rCBX3j5PGoy3MROYOjZ jazzkid@jazzpc"
   ];
 
   system.stateVersion = "24.11";
