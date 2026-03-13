6a44d4892bae71d12ee008c98bbcbaa95e45ef4d
jazzkid <jazzkid@jazzkid.xyz>
2026-03-13 09:00:55 +0000

    cachix.jazzkid.xyz substituter

--- modules/common/security.nix
+++ modules/common/security.nix
@@ -1,4 +1,9 @@
 { ... }:
 {
-  nix.settings.trusted-public-keys = [ "cachix.jazzkid.xyz:74drntauc/Zv2ihaTqSMYiGE7Rcdt481S6Q0phHDU3c=" ];
+  nix.settings = {
+    substituters= [
+      "https://cachix.jazzkid.xyz"
+    ];
+    trusted-public-keys = [ "cachix.jazzkid.xyz:74drntauc/Zv2ihaTqSMYiGE7Rcdt481S6Q0phHDU3c=" ];
+  };
 }
