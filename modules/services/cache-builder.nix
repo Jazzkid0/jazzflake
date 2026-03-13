3e5b33a4b85ae39855017c8b1281dc59f8ef4498
jazzkid <jazzkid@jazzkid.xyz>
2026-03-13 19:29:37 +0000

    cache builder signatures

--- modules/services/cache-builder.nix
+++ modules/services/cache-builder.nix
@@ -13,6 +13,12 @@ in
     group = "users";
   };
 
+  age.secrets.attic-signing-key = {
+    file = ../../secrets/attic-signing-key.age;
+    owner = "jazzkid";
+    group = "users";
+  };
+
   systemd.services.attic-push = {
     description = "Build and push packages to the attic cache";
     wants = [ "network-online.target" "atticd.service" ];
@@ -27,28 +33,26 @@ in
     script = ''
       set -euo pipefail
 
-      JJ_CONFIG=$(mktemp)
-      echo '
-        [user]
-        name="${gitName}"
-        email="${gitEmail}"
-      ' > $JJ_CONFIG
-
       if [ ! -d "${repoPath}/.git" ]; then
         echo "Cloning packages repo..."
         ${pkgs.jujutsu}/bin/jj git clone ${repoUrl} ${repoPath};
       fi
 
       cd ${repoPath}
-      ${pkgs.jujutsu}/bin/jj git fetch --config-file $JJ_CONFIG
-      ${pkgs.jujutsu}/bin/jj new main --config-file $JJ_CONFIG
+      ${pkgs.jujutsu}/bin/jj git fetch --config user.name=${gitName} --config user.email=${gitEmail}
+      ${pkgs.jujutsu}/bin/jj new main --config user.name=${gitName} --config user.email=${gitEmail}
+      ${pkgs.jujutsu}/bin/jj branch create jazzpkgs --config user.name=${gitName} --config user.email=${gitEmail}
 
       ${pkgs.nix}/bin/nix flake update
-      ${pkgs.jujutsu}/bin/jj commit -m"auto: flake update" --config-file $JJ_CONFIG
+      ${pkgs.jujutsu}/bin/jj commit -m"auto: flake update" --config user.name=${gitName} --config user.email=${gitEmail}
 
       PACKAGES=$(${pkgs.nix}/bin/nix run .#build-all)
 
+      echo "$PACKAGES" | xargs ${pkgs.nix}/bin/nix store sign --key-file ${config.age.secrets.attic-signing-key.path} --recursive
+
       echo "$PACKAGES" | xargs ${pkgs.attic-client}/bin/attic push main
+
+      ${pkgs.jujutsu}/bin/jj git push -b jazzpkgs --config user.name=${gitName} --config user.email=${gitEmail}
 
       echo "packages updated successfully"
     '';
