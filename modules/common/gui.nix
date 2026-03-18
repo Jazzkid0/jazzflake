b64acd0db6e351bcc701031430312ba462aba64a
jazzkid <jazzkid@jazzkid.xyz>
2026-03-18 17:14:49 +0000

    ly display manager

--- modules/common/gui.nix
+++ modules/common/gui.nix
@@ -20,6 +20,10 @@
 
     programs.hyprland.enable = true;
 
+    services.displayManager.ly = {
+      enable = true;
+    };
+
     xdg.portal = {
       enable = true;
       extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
