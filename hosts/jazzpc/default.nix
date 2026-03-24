2d59660a7b818913c9ba7d0e5aab7eff484f69c9
jazzkid <jazzkid@jazzkid.xyz>
2026-03-24 12:04:18 +0000

    key typo

--- hosts/jazzpc/default.nix
+++ hosts/jazzpc/default.nix
@@ -23,7 +23,7 @@
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXr41H8R6YjEFGilGFw3k+KmuPyDaOofxctpQMmY18f jazzkid@jazzsl"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQVZgbcapMI2cVH7/t7WYCcxDbVyQQYIWW4Q51K5CIO jazzkid@jazzserver"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQVZnbcapMI2cVH7/t7WYCcxDbVyQQYIWW4Q51K5CIO jazzkid@jazzserver"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbpMhMLZqxJXZxKF8PsNtjV69h2HbRwA4HFRAURqjB4 jazzkid@jazznas"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7Ri38NEb1cN4BA6xQzsqpGrG1VaiRjZxS+D21UV1RA jazzkid@jazzphone"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCRD3bZT/rqSSmteMz58X1di54tbHiOQTx3G8wXbezQ jazzkid@jazztab"
@@ -36,7 +36,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXr41H8R6YjEFGilGFw3k+KmuPyDaOofxctpQMmY18f jazzkid@jazzsl"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQVZgbcapMI2cVH7/t7WYCcxDbVyQQYIWW4Q51K5CIO jazzkid@jazzserver"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQVZnbcapMI2cVH7/t7WYCcxDbVyQQYIWW4Q51K5CIO jazzkid@jazzserver"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbpMhMLZqxJXZxKF8PsNtjV69h2HbRwA4HFRAURqjB4 jazzkid@jazznas"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIN3rt+uOSH4HrmG/LiNb3r7rCBX3j5PGoy3MROYOjZ jazzkid@jazzpc"
   ];
