db8145f0cfcca4639d0f3e676f0d8a50ffe79ada
jazzkid <jazzkid@jazzkid.xyz>
2026-03-25 12:25:57 +0000

    hyprland scrolling layout

--- users/jazzkid/hyprland.nix
+++ users/jazzkid/hyprland.nix
@@ -19,6 +19,17 @@
         gaps_in = 4;
         gaps_out = 8;
         border_size = 2;
+        layout = "scrolling";
+      };
+
+      scrolling = {
+        direction = "right";
+        fullscreen_on_one_column = true;
+        column_width = 0.5;
+        focus_fit_method = 1;
+        follow_focus = true;
+        follow_min_visible = 0.4;
+        explicit_column_widths = "0.333, 0.5, 0.667, 1.0";
       };
 
       decoration = {
