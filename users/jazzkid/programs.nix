9c2c8dd38615ca876a724319fee3142fdaf52612
jazzkid <jazzkid@jazzkid.xyz>
2026-04-02 02:40:05 +0100

    ssh module

--- users/jazzkid/programs.nix
+++ users/jazzkid/programs.nix
@@ -1,6 +1,7 @@
 {...}: {
   imports = [
     ./zsh.nix
+    ./ssh.nix
     ./git.nix
     ./starship.nix
     ./tmux.nix
@@ -11,41 +12,4 @@
     ./htop.nix
   ];
 
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    matchBlocks = {
-      "*" = {
-        forwardAgent = true;
-      };
-      "github_jazzkid" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519_jazzkid";
-        identitiesOnly = true;
-      };
-      "github_public" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519_public";
-        identitiesOnly = true;
-      };
-      "jazzserver" = {
-        hostname = "dev.jazzkid.xyz";
-        user = "jazzkid";
-      };
-      "dev.jazzkid.xyz" = {
-        hostname = "dev.jazzkid.xyz";
-        user = "jazzkid";
-      };
-      "jazznas" = {
-        hostname = "nas.jazzkid.xyz";
-        user = "jazzkid";
-      };
-      "nas.jazzkid.xyz" = {
-        hostname = "nas.jazzkid.xyz";
-        user = "jazzkid";
-      };
-    };
-  };
 }
---
+++ users/jazzkid/ssh.nix
@@ -0,0 +1,45 @@
+{...}: {
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks = {
+      "*" = {
+        forwardAgent = true;
+      };
+      "github_jazzkid" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519_jazzkid";
+        identitiesOnly = true;
+      };
+      "github_public" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519_public";
+        identitiesOnly = true;
+      };
+      "jazzserver" = {
+        hostname = "dev.jazzkid.xyz";
+        user = "jazzkid";
+      };
+      "dev.jazzkid.xyz" = {
+        hostname = "dev.jazzkid.xyz";
+        user = "jazzkid";
+      };
+      "jazznas" = {
+        hostname = "nas.jazzkid.xyz";
+        user = "jazzkid";
+      };
+      "nas.jazzkid.xyz" = {
+        hostname = "nas.jazzkid.xyz";
+        user = "jazzkid";
+      };
+      "git_nas" = {
+        hostname = "nas.jazzkid.xyz";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+        identitiesOnly = true;
+      };
+    };
+  };
+}
