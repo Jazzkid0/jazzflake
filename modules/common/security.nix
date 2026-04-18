dff0c11f4dd98126a58ab8f839a55950d4f04e9a
jazzkid <jazzkid@jazzkid.xyz>
2026-04-18 04:52:24 +0100

    cache trusted public key

--- modules/common/security.nix
+++ modules/common/security.nix
@@ -15,7 +15,10 @@
     substituters = [
       "https://cachix.jazzkid.xyz/main"
     ];
-    trusted-public-keys = ["cachix.jazzkid.xyz:74drntauc/Zv2ihaTqSMYiGE7Rcdt481S6Q0phHDU3c="];
+    trusted-public-keys = [
+      "cachix.jazzkid.xyz:74drntauc/Zv2ihaTqSMYiGE7Rcdt481S6Q0phHDU3c=" # might be wrong?
+      "main:1XdAi4NruvfL1KT6MELYdyd9zSCenzA0pRupKxOMtHQ=" # as stated on jazznas
+    ];
     netrc-file = config.age.secrets.nix-netrc.path;
   };
 }
