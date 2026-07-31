5e7180f665bc1f79cf59084929fed2f4dc571e1f
jazzkid <jazzkid@jazzkid.xyz>
2026-08-01 00:37:50 +0100

    agent: rewrite security.nix for extra-substituters + watch-store + attic client config

--- modules/common/security.nix
+++ modules/common/security.nix
@@ -1,25 +1,47 @@
 {
   config,
+  lib,
+  pkgs,
   sshKeys,
   ...
-}: {
-  age.secrets.nix-netrc = {
-    file = ../../secrets/nix-netrc.age;
-    owner = "jazzkid";
-    group = "users";
-    mode = "0400";
-  };
-
+}: let
+  pushHosts = [ "jazznas" "jazzpc" ];
+  isPushHost = builtins.elem config.networking.hostName pushHosts;
+in {
   users.users.root.openssh.authorizedKeys.keys = with sshKeys; [jazzsl jazzserver jazznas jazzpc];
 
   nix.settings = {
-    substituters = [
-      "https://cachix.jazzkid.xyz/main"
-    ];
-    trusted-public-keys = [
-      "cachix.jazzkid.xyz:74drntauc/Zv2ihaTqSMYiGE7Rcdt481S6Q0phHDU3c=" # might be wrong?
-      "main:1XdAi4NruvfL1KT6MELYdyd9zSCenzA0pRupKxOMtHQ=" # as stated on jazznas
-    ];
-    netrc-file = config.age.secrets.nix-netrc.path;
+    extra-substituters = [ "https://cachix.jazzkid.xyz/main" ];
+    trusted-substituters = [ "https://cachix.jazzkid.xyz/main" ];
+    extra-trusted-public-keys = [ "TODO:paste-key-from-atticd-atticadm-cache-info" ];
+  };
+
+  age.secrets.attic-push-token = lib.mkIf isPushHost {
+    file = ../../secrets/attic-push-token.age;
+    owner = "jazzkid";
+    group = "users";
+    mode = "0440";
+  };
+
+  environment.etc."attic/config.toml" = lib.mkIf isPushHost {
+    text = ''
+      default-server = "jazznas"
+      [servers.jazznas]
+      endpoint = "https://cachix.jazzkid.xyz/"
+      token-file = "${config.age.secrets.attic-push-token.path}"
+    '';
+    mode = "0444";
+  };
+
+  systemd.services.attic-watch-store = lib.mkIf isPushHost {
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.attic-client}/bin/attic watch-store main";
+      Restart = "on-failure";
+      RestartSec = 30;
+    };
+    environment.XDG_CONFIG_HOME = "/etc";
   };
 }
