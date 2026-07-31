691700a33c0a095330592baed85ba515fec4fbde
jazzkid <jazzkid@jazzkid.xyz>
2026-08-01 00:37:50 +0100

    agent: update secrets for managed signing (add push-token + deploy-key, remove old secrets)

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,16 @@
 let
   sshKeys = import ../modules/common/ssh-keys.nix;
   keys = with sshKeys; [jazzsl jazzserver jazznas jazzpc];
+  jazznasKeys = with sshKeys; [jazzsl jazznas];
+  pushHostKeys = with sshKeys; [jazzsl jazznas jazzpc];
 in {
   "cloudflare_apiTokenFile.age".publicKeys = keys;
   "transmission_credentialsFile.age".publicKeys = keys;
   "silverbullet_credentialsFile.age".publicKeys = keys;
   "qbittorrent_conf.age".publicKeys = keys;
-  "attic-environment-file.age".publicKeys = keys;
-  "attic-signing-key.age".publicKeys = keys;
-  "attic-token.age".publicKeys = keys;
-  "nix-netrc.age".publicKeys = keys;
+  "attic-environment-file.age".publicKeys = jazznasKeys;
+  "attic-push-token.age".publicKeys = pushHostKeys;
+  "github-deploy-key.age".publicKeys = jazznasKeys;
   "samba-creds.age".publicKeys = keys;
   "opencode-env.age".publicKeys = keys;
 }
