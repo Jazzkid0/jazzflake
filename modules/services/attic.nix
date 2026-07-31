ce00e3f3bed1a9378dc42943da1893674c4eb7f3
jazzkid <jazzkid@jazzkid.xyz>
2026-08-01 00:12:41 +0100

    agent: rewrite attic.nix for managed signing + RS256 + cache init

--- modules/services/attic.nix
+++ modules/services/attic.nix
@@ -19,10 +19,6 @@
     group = "atticd";
   };
 
-  age.secrets.attic-signing-key = {
-    file = ../../secrets/attic-signing-key.age;
-  };
-
   systemd.tmpfiles.rules = [
     "d /data/nix-cache 0755 atticd atticd -"
     "f /var/lib/private/atticd/db.sqlite3 0644 atticd atticd -"
@@ -38,14 +34,16 @@
 
       jwt = {};
 
+      "allowed-hosts" = [ "cachix.jazzkid.xyz" ];
+      "api-endpoint" = "https://cachix.jazzkid.xyz/";
+
       storage = {
         type = "local";
         path = "/data/nix-cache";
       };
 
-      # TODO: Tune these later
       chunking = {
-        nar-size-threshold = 65536; # chunk @ >64KiB
+        nar-size-threshold = 65536;
         min-size = 16384;
         avg-size = 65536;
         max-size = 262144;
@@ -55,7 +53,34 @@
         interval = "12 hours";
         default-retention-period = "6 months";
       };
+
+      compression = {
+        type = "zstd";
+      };
     };
+  };
+
+  systemd.services.atticd.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    ReadWritePaths = [ "/data/nix-cache" ];
+    RequiresMountsFor = [ "/data" ];
+  };
+
+  systemd.services.attic-init = {
+    after = [ "atticd.service" ];
+    requires = [ "atticd.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    path = [ pkgs.curl config.services.atticd.package ];
+    script = ''
+      until curl -s http://[::1]:6463/api/v1/server-info > /dev/null; do sleep 1; done
+      if ! atticd-atticadm cache info main 2>/dev/null; then
+        atticd-atticadm cache create main --public --priority 39
+      fi
+    '';
   };
 
   services.nginx.virtualHosts."cachix.jazzkid.xyz" = {
@@ -70,14 +95,9 @@
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
 
-        # avoid choking nginx with large NARs
         client_max_body_size 8G;
         proxy_request_buffering off;
       '';
     };
-  };
-
-  nix.settings = {
-    secret-key-files = [config.age.secrets.attic-signing-key.path];
   };
 }
