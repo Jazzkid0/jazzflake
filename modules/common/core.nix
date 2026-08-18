3af57c32a0bed8f7ba36e54815b86138948ddf51
jazzkid <jazzkid@jazzkid.xyz>
2026-08-18 11:01:18 +0100

    auto-optimise the nix store

--- modules/common/core.nix
+++ modules/common/core.nix
@@ -11,7 +11,20 @@
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = ["nix-command" "flakes"];
+    };
+    optimise = {
+      automatic = true;
+      dates = [
+        "Tue 04:30"
+      ];
+      randomizedDelaySec = "1800";
+    };
+    # configuration.nix
+  };
 
   users.users.root.openssh.authorizedKeys.keys = with sshKeys; [jazzsl jazzserver jazznas jazzpc];
 
